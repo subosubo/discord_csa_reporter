344aa8061b2f58abc5599782ac7b113b1792beaa
subosubo <subo0sub0sub0@gmail.com>
2022-11-25 19:44:55 +0800

    class clean

--- csa.py
+++ csa.py
@@ -2,10 +2,12 @@ import datetime
 import json
 import logging
 import pathlib
+import sys
 from os.path import join
 from bs4 import BeautifulSoup
 
 import pytz
+import yaml
 import requests
 from discord import Color, Embed, HTTPException
 import os
@@ -14,21 +16,37 @@ utc = pytz.UTC
 
 
 class csa_report:
-
-    def __init__(self, valid, keywords, keywords_i, product, product_i):
-        self.valid = valid
-        self.keywords = keywords
-        self.keywords_i = keywords_i
-        self.product = product
-        self.product_i = product_i
+    def __init__(self):
 
         self.CSA_URL = "https://www.csa.gov.sg"
         self.PUBLISH_JSON_PATH = join(
-            pathlib.Path(__file__).parent.absolute(), "output/record.json")
+            pathlib.Path(__file__).parent.absolute(), "output/record.json"
+        )
         self.CSA_TIME_FORMAT = "%d %b %Y"
-        self.CSA_CREATED = datetime.datetime.now(utc) - datetime.timedelta(
-            days=1)
+        self.CSA_CREATED = datetime.datetime.now(utc) - datetime.timedelta(days=1)
         self.logger = logging.getLogger("csa-reporter")
+
+        self.new_alerts = []
+        self.new_alerts_title = []
+
+        # Load keywords from config file
+        self.KEYWORDS_CONFIG_PATH = join(
+            pathlib.Path(__file__).parent.absolute(), "config/config.yaml"
+        )
+        try:
+
+            with open(self.KEYWORDS_CONFIG_PATH, "r") as yaml_file:
+                keywords_config = yaml.safe_load(yaml_file)
+                print(f"Loaded keywords: {keywords_config}")
+                self.valid = keywords_config["ALL_VALID"]
+                self.keywords_i = keywords_config["DESCRIPTION_KEYWORDS_I"]
+                self.keywords = keywords_config["DESCRIPTION_KEYWORDS"]
+                self.product_i = keywords_config["PRODUCT_KEYWORDS_I"]
+                self.product = keywords_config["PRODUCT_KEYWORDS"]
+
+        except Exception as e:
+            self.logger.error(e)
+            sys.exit(1)
 
     ################## LOAD CONFIGURATIONS ####################
 
@@ -40,7 +58,8 @@ class csa_report:
             with open(self.PUBLISH_JSON_PATH, "r") as json_file:
                 csa_time = json.load(json_file)
                 self.CSA_CREATED = datetime.datetime.strptime(
-                    csa_time["CREATED"], self.CSA_TIME_FORMAT)
+                    csa_time["CREATED"], self.CSA_TIME_FORMAT
+                )
 
         except Exception as e:  # If error, just keep the fault date (today - 1 day)
             self.logger.error(f"ERROR: {e}")
@@ -54,8 +73,7 @@ class csa_report:
             with open(self.PUBLISH_JSON_PATH, "w") as json_file:
                 json.dump(
                     {
-                        "CREATED": self.CSA_CREATED.strftime(
-                            self.CSA_TIME_FORMAT),
+                        "CREATED": self.CSA_CREATED.strftime(self.CSA_TIME_FORMAT),
                     },
                     json_file,
                 )
@@ -74,14 +92,18 @@ class csa_report:
                 soup = BeautifulSoup(r.text, "lxml")
                 for elem in soup.select(".sc-card-block"):
                     result = {}
-                    result['csa'] = f"{self.CSA_URL}{elem.get('href')}"
-                    result['title'] = elem.find(
-                        class_="sc-card-title").get_text(" ", strip=True)
-                    result['description'] = elem.find(
-                        "p", class_="sc-card-desc").get_text(" ", strip=True)
-                    result['created'] = elem.find(
-                        "div", class_="sc-card-publish").get_text(
-                            " ", strip=True).split(" on ")[1]
+                    result["csa"] = f"{self.CSA_URL}{elem.get('href')}"
+                    result["title"] = elem.find(class_="sc-card-title").get_text(
+                        " ", strip=True
+                    )
+                    result["description"] = elem.find(
+                        "p", class_="sc-card-desc"
+                    ).get_text(" ", strip=True)
+                    result["created"] = (
+                        elem.find("div", class_="sc-card-publish")
+                        .get_text(" ", strip=True)
+                        .split(" on ")[1]
+                    )
                     results.append(result)
 
                 return results
@@ -89,17 +111,17 @@ class csa_report:
             self.logger.error(f"{e}")
             os.system("kill 1")
 
-    def filter_alerts(self, alerts, last_create: datetime.datetime):
+    def filter_alerts(self, alerts: list, last_create: datetime.datetime):
 
         filtered_alerts = []
         new_last_time = last_create
 
         for alert in alerts:
-            alert_time = datetime.datetime.strptime(alert["created"],
-                                                    self.CSA_TIME_FORMAT)
+            alert_time = datetime.datetime.strptime(
+                alert["created"], self.CSA_TIME_FORMAT
+            )
             if alert_time > last_create:
-                if self.valid or self.is_summ_keyword_present(
-                        alert["description"]):
+                if self.valid or self.is_summ_keyword_present(alert["description"]):
                     filtered_alerts.append(alert)
 
             if alert_time > new_last_time:
@@ -117,27 +139,25 @@ class csa_report:
     def get_new_alerts(self):
 
         alerts = self.get_alerts()
-        #print(alerts)
-        filtered_alerts, new_last_time = self.filter_alerts(
-            alerts, self.CSA_CREATED)
-        self.CSA_CREATED = new_last_time
+        self.new_alerts, self.CSA_CREATED = self.filter_alerts(alerts, self.CSA_CREATED)
 
-        return filtered_alerts
+        self.new_alerts_title = [new_alert["title"] for new_alert in self.new_alerts]
+        print(f"CSA Alerts: {self.new_alerts_title}")
+        self.logger.info(f"CSA Alerts: {self.new_cves_ids}")
 
     def generate_new_alert_message(self, new_alerts) -> Embed:
-        # Generate new CVE message for sending to slack
-        nl = "\n"
+        # Generate new CVE message for sending to discord
         embed = Embed(
             title=f"🔈 *{new_alerts['title']}*",
             description=new_alerts["description"]
-            if len(new_alerts["description"]) < 500 else
-            new_alerts["description"][:500] + "...",
+            if len(new_alerts["description"]) < 500
+            else new_alerts["description"][:500] + "...",
             timestamp=datetime.datetime.utcnow(),
             color=Color.light_gray(),
         )
-        embed.add_field(name=f"📅  *Published*",
-                        value=f"{new_alerts['created']}",
-                        inline=True)
+        embed.add_field(
+            name=f"📅  *Published*", value=f"{new_alerts['created']}", inline=True
+        )
         embed.add_field(
             name=f"More Information",
             value=f"{new_alerts['csa']}",
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 import logging
 import os
-import pathlib
-import sys
 from os.path import join
+from time import sleep
 
 from os.path import join, dirname
 from dotenv import load_dotenv
 import aiohttp
-import yaml
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from csa import csa_report
 from discord import Embed, HTTPException, Webhook
@@ -37,36 +35,6 @@ streamhandler.setLevel(logging.INFO)
 streamhandler.setFormatter(formatter)
 log.addHandler(streamhandler)
 
-#################### LOADING #########################
-
-
-def load_keywords():
-    # Load keywords from config file
-    KEYWORDS_CONFIG_PATH = join(
-        pathlib.Path(__file__).parent.absolute(), "config/config.yaml"
-    )
-    try:
-
-        with open(KEYWORDS_CONFIG_PATH, "r") as yaml_file:
-            keywords_config = yaml.safe_load(yaml_file)
-            print(f"Loaded keywords: {keywords_config}")
-            ALL_VALID = keywords_config["ALL_VALID"]
-            DESCRIPTION_KEYWORDS_I = keywords_config["DESCRIPTION_KEYWORDS_I"]
-            DESCRIPTION_KEYWORDS = keywords_config["DESCRIPTION_KEYWORDS"]
-            PRODUCT_KEYWORDS_I = keywords_config["PRODUCT_KEYWORDS_I"]
-            PRODUCT_KEYWORDS = keywords_config["PRODUCT_KEYWORDS"]
-
-            return (
-                ALL_VALID,
-                DESCRIPTION_KEYWORDS,
-                DESCRIPTION_KEYWORDS_I,
-                PRODUCT_KEYWORDS,
-                PRODUCT_KEYWORDS_I,
-            )
-    except Exception as e:
-        log.error(e)
-        sys.exit(1)
-
 
 #################### SEND MESSAGES #########################
 
@@ -90,40 +58,21 @@ async def sendtowebhook(webhookurl: str, content: Embed):
             await webhook.send(embed=content)
 
         except HTTPException:
-            log.debug("http error")
-            os.system("kill 1")
+            sleep(180)
+            await webhook.send(embed=content)
 
 
 #################### MAIN BODY #########################
 async def itscheckintime():
 
-    (
-        ALL_VALID,
-        DESCRIPTION_KEYWORDS,
-        DESCRIPTION_KEYWORDS_I,
-        PRODUCT_KEYWORDS,
-        PRODUCT_KEYWORDS_I,
-    ) = load_keywords()
-
-    csa = csa_report(
-        ALL_VALID,
-        DESCRIPTION_KEYWORDS,
-        DESCRIPTION_KEYWORDS_I,
-        PRODUCT_KEYWORDS,
-        PRODUCT_KEYWORDS_I,
-    )
+    csa = csa_report()
     csa.load_lasttimes()
     new_alerts = csa.get_new_alerts()
 
-    alert_title = [new_alert["title"] for new_alert in new_alerts]
-    print(f"CSA Alerts: {alert_title}")
-
-    for alert in new_alerts:
-        alert_msg = csa.generate_new_alert_message(alert)
-        print("passed")
-        await send_discord_message(alert_msg)
-
-    # print("passed")
+    if csa.new_alerts:
+        for alert in new_alerts:
+            alert_msg = csa.generate_new_alert_message(alert)
+            await send_discord_message(alert_msg)
 
     csa.update_lasttimes()
 
